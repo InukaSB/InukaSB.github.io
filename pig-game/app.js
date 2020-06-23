469aa1c446d73d33fcec42750da85ed89b992f98
inukasb <inukasanudayab@gmail.com>
2020-06-23 21:19:22 +0530

    added pig game

---
+++ pig-game/app.js
@@ -0,0 +1,116 @@
+/*
+
+GAME RULES: 
+
+- the game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times he wishes. Each result get added to his round score.
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that it's the next player's turn. 
+- The player can choose to 'Hold', witch means that his ROUND score gets added to his GLOBAL score. After that, it's the next players turn.
+- the first player to reach 100 points on GLOBAL score wins the game.
+
+*/
+
+
+
+var scores, roundScore, activePlayer, gamePlaying;
+
+init();
+
+document.querySelector('.wrapper').style.display = 'none';
+
+document.querySelector('.start-game').addEventListener('click', function() {
+    document.querySelector('.rules').style.display = 'none';
+    document.querySelector('.wrapper').style.display = 'block';
+});
+
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+
+    if(gamePlaying) {
+        // 1. Random number
+        var dice = Math.floor(Math.random() * 6) + 1;
+
+        //2. Display the result
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
+
+        //3. Update the round score If NOT a 1
+        if (dice !== 1) {
+            //add score
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+         
+        } else {
+            // next player
+            nextPlayer();
+        }
+    }
+
+
+});
+
+
+
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    
+    if(gamePlaying) {
+        // Add CURRENT score to GLOBAL score
+        scores[activePlayer] += roundScore;
+
+        // Update the UI
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+        // Check if player won the game
+        if (scores[activePlayer] >= 100) {
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            gamePlaying = false;
+        } else {
+            //Next player
+            nextPlayer();
+        }
+    }
+});
+
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    // document.querySelector('.dice').style.display = 'none';
+}
+
+// resets the game when clicked
+document.querySelector('.btn-new').addEventListener('click', init);
+
+// Resetting the game
+function init() {
+    
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+    gamePlaying = true;
+
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0'; 
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+}
